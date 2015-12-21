f2844c4d9e386b3fb52f47862a902f749da42d8c
Daren Wang <d227wang@uwaterloo.ca>
2015-12-21 11:48:53 -0700

    change html file name

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,4 +1,12 @@
-function replaceText (){
-    document.getElementById("quoteBox").innerHTML = "TEST";
 
+$('a#next').click(
+
+    function replaceText() {
+        document.getElementById("quoteBox").innerHTML = "TEST";
+
+    }
+)
+
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
 }
